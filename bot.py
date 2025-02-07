6dbd5a2577927ed0890f1bbdd564014d10980a1d
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-02-07 14:59:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -218,6 +218,7 @@ def main():
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("broadcast", broadcast))
     application.add_handler(CommandHandler("broadcast_api", broadcast_api))
+  application.add_handler(CommandHandler("connect", connect))
     application.add_handler(CommandHandler("disconnect", disconnect))
     application.add_handler(CommandHandler("commands", commands))
     application.add_handler(CommandHandler("view", view))
