ed0d6d37e6cd4d3734c4ad6688bfd543dc62673d
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-02-08 22:51:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import socket
 import threading
 import requests
 from telegram import Update
-from telegram.ext import Application, Filters, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import filters
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
